04f88ecc01913d1a35318b815865026809cdbdb6
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-14 15:58:03 +0300

    Server is ready

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import makeStrore from './src/store.js';
 import startServer from './src/server.js';
+const initialEntries = require('./entries.json');
 
 export const store = makeStrore();
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: initialEntries,
+});
+
+store.dispatch({
+  type: 'NEXT',
+});
 
